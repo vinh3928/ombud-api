94a8253180ff31985065ab5bb72701e1bd7e7717
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-11-07 12:09:58 -0700

    added user to params

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 var axios = require('axios');
 
 /* GET home page. */
-router.get('/commits/:name', function(req, res, next) {
+router.get('/commits/:name/:user', function(req, res, next) {
   axios({
     method: "get",
-    url: "https://api.github.com/repos/vinh3928/" + req.params.name + "/commits?client_id=" + process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET
+    url: "https://api.github.com/repos/" + req.params.user + "/" + req.params.name + "/commits?client_id=" + process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET
   })
   .then(function(data) {
     res.status(200).json(data);
